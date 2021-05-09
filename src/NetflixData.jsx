dead2f59164fcc8c21fe17b45bb3293e9f9559d8
Shivain <83839735+shivain07@users.noreply.github.com>
2021-05-09 13:00:04 +0530

    Update NetflixData.jsx

--- src/NetflixData.jsx
+++ src/NetflixData.jsx
@@ -4,146 +4,159 @@ import React from 'react'
 let img= 400
 const NetflixData =[
 {
-    image :`/images/ih1.jpeg`,
-    //  show :'ready',
-     type :'horror'
-}
-,
+image:"https://source.unsplash.com/1600x900/?horror",
 
-{
-    image :`/images/ih2.jpeg`,
-    //  show :'Brake',
-     type :'horror'
-}
-,
-{
-    image :`/images/ih3.jpeg`,
-    //  show :'Brake',
+    //  show :'ready',
      type :'horror'
 },
 {
-    image :`/images/ih4.jpeg`,
-    //  show :'Brake',
+image:"https://source.unsplash.com/1600x900/?horror,ghost,1",
+
+    //  show :'ready',
      type :'horror'
-}
-,
+},
 {
-    image :`/images/ac3.jpeg`,
-    //  show :'Brake',
+image:"https://source.unsplash.com/1600x900/?horror,ghost,2",
+
+    //  show :'ready',
      type :'horror'
-}
+},
+{
+image:"https://source.unsplash.com/1600x900/?horror,ghost,3",
+
+    //  show :'ready',
+     type :'horror'
+},
+{
+image:"https://source.unsplash.com/1600x900/?horror,ghost,4",
+
+    //  show :'ready',
+     type :'horror'
+},
+{
+image:"https://source.unsplash.com/1600x900/?horror,ghost,5",
+
+    //  show :'ready',
+     type :'horror'
+},
+{
+image:"https://source.unsplash.com/1600x900/?horror,ghost,6",
+
+    //  show :'ready',
+     type :'horror'
+},
+
 
 
 ,
 {
-    image :`/images/ac1.jpeg`,
+    image :`https://source.unsplash.com/1600x900/?fight,action`,
     //  show :'Brake',
      type :'action'
 }
 ,
 {
-    image :`/images/ac2.jpeg`,
+    image :`https://source.unsplash.com/1600x900/?voilence,action`,
     //  show :'Brake',
      type :'action'
 }
 ,
 {
-    image :`/images/ac3.jpeg`,
+    image :`https://source.unsplash.com/1600x900/?gun,action`,
     //  show :'Brake',
      type :'action'
 }
 ,
 {
-    image :`/images/i1.jpeg`,
+    image :`https://source.unsplash.com/1600x900/?punch,action`,
     //  show :'Brake',
      type :'action'
 }
 ,
 {
-    image :`/images/ac4.jpeg`,
+    image :`https://source.unsplash.com/1600x900/?boxing,action`,
     //  show :'Brake',
      type :'action'
 }
 ,
 {
-    image :`/images/ac5.jpeg`,
+    image :`https://source.unsplash.com/1600x900/?judo,action`,
     //  show :'Brake',
      type :'action'
 }
 
 
+
 ,
 {
-    image :`/images/i4.jpeg`,
+    image :'https://source.unsplash.com/1600x900/?funny,joker',
     //  show :'Brake',
      type :'comedy'
 }
 ,
 {
-    image :`/images/i5.jpeg`,
+    image :'https://source.unsplash.com/1600x900/?joker,1',
+    //  show :'Brake',
+     type :'comedy'
+}
+,
+{
+    image :'https://source.unsplash.com/1600x900/?joker,2',
+    //  show :'Brake',
+     type :'comedy'
+}
+,
+{
+    image :'https://source.unsplash.com/1600x900/?joker,3',
+    //  show :'Brake',
+     type :'comedy'
+}
+,
+{
+    image :'https://source.unsplash.com/1600x900/?joker,4',
+    //  show :'Brake',
+     type :'comedy'
+}
+,
+{
+    image :'https://source.unsplash.com/1600x900/?joker,5',
     //  show :'Brake',
      type :'comedy'
 }
 
 ,
 {
-    image :`/images/i7.jpeg`,
-    //  show :'Brake',
-     type :'comedy'
-}
-,
-{
-    image :`/images/ic1.jpeg`,
-    //  show :'Brake',
-     type :'comedy'
-}
-,
-{
-    image :`/images/ic2.jpeg`,
-    //  show :'Brake',
-     type :'comedy'
-}
-
-,
-{
-    image :`/images/ic3.jpeg`,
-    //  show :'Brake',
-     type :'comedy'
-}
-,
-{
-    image :`/images/i8.jpeg`,
-    //  show :'Brake',
-     type :'comedy'
-}
-,
-{
-    image :`/images/i7.jpeg`,
+    image :`https://source.unsplash.com/1600x900/?romance`,
     //  show :'Brake',
      type :'romance'
 }
 ,
 {
-    image :`/images/i8.jpeg`,
-    //  show :'Brake',
-     type :'romance'
-}
-
-,
-{
-    image :`/images/ic2.jpeg`,
+    image :`https://source.unsplash.com/1600x900/?romance,1`,
     //  show :'Brake',
      type :'romance'
 }
 ,
 {
-    image :`/images/i6.jpeg`,
+    image :`https://source.unsplash.com/1600x900/?romance,2`,
     //  show :'Brake',
      type :'romance'
 }
 ,
 {
-    image :`/images/ac4.jpeg`,
+    image :`https://source.unsplash.com/1600x900/?romance,3`,
+    //  show :'Brake',
+     type :'romance'
+}
+,
+{
+    image :`https://source.unsplash.com/1600x900/?romance,4`,
+    //  show :'Brake',
+     type :'romance'
+}
+,
+{
+    image :`https://source.unsplash.com/1600x900/?romance,5`,
     //  show :'Brake',
      type :'romance'
 }
